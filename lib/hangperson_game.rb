ca333206e8183c36be2fba86075988a0073d3988
Lukasz Gaza <g.lukasz@gmail.com>
2018-01-11 18:22:17 +0100

    Implemented the game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,48 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  def guess(letter)
+    raise ArgumentError if letter !~ /[a-z]+/i
+
+    letter = letter.downcase
+    return false if @guesses.include? letter or @wrong_guesses.include? letter
+
+    if @word.include? letter
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+
+    true
+  end
+
+  def word_with_guesses
+    result = ""
+
+    @word.each_char { |c|
+      if @guesses.include? c
+        result += c
+      else
+        result += "-"
+      end
+    }
+
+    result
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    return :play if word_with_guesses.include? "-"
+    :win
   end
 
   def self.get_random_word
